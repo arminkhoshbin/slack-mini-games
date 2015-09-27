1c829bf7872ddd5b267ffcea9c7e027be51b3e4d
Armin Khoshbin <armin.khoshbin@gmail.com>
2015-09-28 01:09:13 +1000

    updated listen

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.post('/roll', function (req, res) {
   //res.send(Roller.start());
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
