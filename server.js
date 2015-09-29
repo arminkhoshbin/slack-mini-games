f0b857dfcb7f4d023543a40133a1537d18ba8cff
Armin Khoshbin <armin.khoshbin@gmail.com>
2015-09-29 15:04:20 +1000

    rock paper scissors game implementation

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ if (typeof process.env.WEBHOOK_URL === 'undefined') {
 	webhook = process.env.WEBHOOK_URL;
 }
 
+var slackUser = 'Slack Mini Games';
+var slackIcon = ':slack:';
+
 var Roller = new (require('./libs/roller'))();
 
 app.get('/', function (req, res) {
@@ -33,9 +36,9 @@ app.post('/roll', function (req, res) {
 
 		var data = JSON.stringify({
 	    text: req.body.user_name + ' rolled number ' + number,
-	    username: 'Slack Mini Games',
+	    username: slackUser,
 	    channel: '#' + req.body.channel_name,
-	    icon_emoji: ":slack:",
+	    icon_emoji: slackIcon,
 	  });
 
 		// send post request to webhook
@@ -64,6 +67,48 @@ app.post('/roll', function (req, res) {
 		res.send('Invalid Token!');
 	}
   
+});
+
+app.post('/rps', function (req, res) {
+	var items = ['Rock', 'Paper', 'Scissors'];
+
+	var slackToken = req.body.token;
+
+	if (process.env.RPS_TOKEN == slackToken || config.RPS_TOKEN == slackToken) {
+
+		var data = JSON.stringify({
+	    text: items[Math.floor(Math.random() * items.length)] + '!',
+	    username: slackUser,
+	    channel: '#' + req.body.channel_name,
+	    icon_emoji: slackIcon,
+	  });
+
+		// send post request to webhook
+	  var req = https.request({
+	   	host: req.body.team_domain + '.slack.com',
+	    path: webhook,
+	    method: 'POST',
+	    headers: {
+	      'Content-Type': 'application/json',
+	      'Content-Length': data.length,
+	    }
+	  }, function(res){
+	      res.setEncoding('utf8');
+	      res.on('data', function(chunk){
+	        console.log('response', chunk);
+	      });
+	 	});
+
+	 	// write data to request body
+		req.write(data);
+		req.end();
+
+		res.end('');
+
+	} else {
+		res.send('Invalid Token!');
+	}
+
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {
